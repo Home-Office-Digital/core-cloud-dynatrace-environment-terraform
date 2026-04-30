6bfee1e6d1bbc7aaf70d26d3737c3635e4e5ac57
Becky <93141344+beckywhitemartin@users.noreply.github.com>
2026-04-30 13:51:28 +0100

    feat: CC-1244 | updates zookeeper rules to include tags (#265)
    * feat: CC-1244 | updates zookeeper rules to include tags
    
    * feat: CC-1244 | updates zookeeper rules to include tags
    
    * feat: CC-1244 | updates zookeeper rules to include tags
    
    * feat: CC-1244 | updates zookeeper rules to include tags

--- dynatrace_management_zones/locals.tf
+++ dynatrace_management_zones/locals.tf
@@ -1,21 +1,23 @@
 locals {
   default_rules_raw = yamldecode(
     templatefile("${path.module}/rules.tftpl", {
-      project_id                   = var.project_id
-      env_name                     = var.zone_vars.env_name
-      tag_env_name                 = var.zone_vars.env_name
-      service_id                   = var.zone_vars.service_id
-      webapp_prefix                = var.zone_vars.webapp_prefix
-      k8s_cluster_name_begins_with = var.zone_vars.k8s_cluster_name_begins_with
-      k8s_cluster_name             = var.zone_vars.k8s_cluster_name
-      k8s_cluster_env              = var.zone_vars.k8s_cluster_env 
-      k8s_namespace                = var.zone_vars.k8s_namespace
-      host_group_begins_with       = var.zone_vars.host_group_begins_with
-      aws_account_id               = var.zone_vars.aws_account_id
-      project_service              = var.zone_vars.project_service
-      pg_to_host_propagation       = var.zone_vars.pg_to_host_propagation
-      pg_to_service_propagation    = var.zone_vars.pg_to_host_propagation
-      activegate_id_pattern        = var.zone_vars.activegate_id_pattern
+      project_id                              = var.project_id
+      env_name                                = var.zone_vars.env_name
+      tag_env_name                            = var.zone_vars.env_name
+      service_id                              = var.zone_vars.service_id
+      webapp_prefix                           = var.zone_vars.webapp_prefix
+      k8s_cluster_name_begins_with            = var.zone_vars.k8s_cluster_name_begins_with
+      k8s_cluster_name                        = var.zone_vars.k8s_cluster_name
+      k8s_cluster_env                         = var.zone_vars.k8s_cluster_env
+      k8s_namespace                           = var.zone_vars.k8s_namespace
+      host_group_begins_with                  = var.zone_vars.host_group_begins_with
+      aws_account_id                          = var.zone_vars.aws_account_id
+      project_service                         = var.zone_vars.project_service
+      pg_to_host_propagation                  = var.zone_vars.pg_to_host_propagation
+      pg_to_service_propagation               = var.zone_vars.pg_to_host_propagation
+      activegate_id_pattern                   = var.zone_vars.activegate_id_pattern
+      process_group_predefined_metadata_regex = var.zone_vars.process_group_predefined_metadata_regex
+      activegate_metrics                      = var.zone_vars.activegate_metrics
     })
   ).default_rules
   rules_templates = length(coalesce(try(var.zone_vars.rules_templates, null), [])) > 0 ? var.zone_vars.rules_templates : var.default_rules
