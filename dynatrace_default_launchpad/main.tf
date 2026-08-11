07bc0f827d35be19b477d81669ed901328fffa78
TOM-CARPENTER-ho <thomas.carpenter@digital.homeoffice.gov.uk>
2026-08-11 15:53:10 +0100

    feat: adding everyone as the default launchpad group

--- dynatrace_default_launchpad/main.tf
+++ dynatrace_default_launchpad/main.tf
@@ -2,8 +2,7 @@ resource "dynatrace_document" "launchpad" {
   type      = "launchpad"
   name      = var.launchpad_name
   content   = jsonencode(var.launchpad_content)
-  custom_id = var.launchpad_custom_id
-  private   = var.launchpad_private
+  private   = false
 }
 
 resource "dynatrace_default_launchpad" "default_launchpad" {
@@ -13,7 +12,7 @@ resource "dynatrace_default_launchpad" "default_launchpad" {
       content {
         is_enabled    = group_launchpad.value.is_enabled
         launchpad_id  = dynatrace_document.launchpad.id
-        user_group_id = group_launchpad.value.user_group_id
+        user_group_id = "d3ffb1e7-4d8f-465e-8b37-98feaa2c1748" #Everyone
       }
     }
   }
